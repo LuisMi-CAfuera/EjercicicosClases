40ec62aad870be40df51fa222bf00cf6e7b68344
LuisMi <luismiguelbravolopez3@gmail.com>
2022-10-02 17:15:08 +0200

    Fin Ejercico Clases2

--- src/Dados.kt
+++ src/Dados.kt
@@ -28,14 +28,6 @@ fun main() {
 
 
 
-
-
-
-
-
-
-
-
 class Dado {
     private var numMin = 1
     private var numMax = 6
@@ -64,15 +56,6 @@ class Dado {
     fun tiradaUnica(): Int {
         return Random.nextInt(numMin, numMax)
     }
-
-    fun tiradaDoble(): Int {
-        val num1 = Random.nextInt(numMin, numMax)
-        println("num1 = $num1")
-        val num2 = Random.nextInt(numMin, numMax)
-        println("num2 = $num2")
-        return if (num1 == num2) num1 * num2 else num1 + num2
-    }
-
 
 }
 
